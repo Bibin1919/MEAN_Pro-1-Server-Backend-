eaa170333e4eeda4a9747297c310ce1b7a0b54f7
Bibin1919 <bibinbinoy1919@gmail.com>
2023-06-14 12:37:36 +0530

    updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000 || process.env.PORT;
 require('dotenv').config();
 require('./db/connection');
 const router = require('./routes/router');
